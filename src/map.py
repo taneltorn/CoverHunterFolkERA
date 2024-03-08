f556a4be66d7d37e0f997e61b73e70ef2e7572ec
Alan Ng <15185920+alanngnet@users.noreply.github.com>
2024-03-07 23:00:48 -0600

    documentation
    based on reading the Ren Jie Tan blog post cited in the original code comments, I corrected and clarified the English explanation of the calc_map function.

--- src/map.py
+++ src/map.py
@@ -11,10 +11,10 @@ import numpy as np
 
 def calc_map(array2d: np.ndarray, label_query: List, label_ref: List,
              topk: int = 10, verbose: int = 0) -> Dict:
-  """ calculate map@10, top10 and rank1
+  """ calculate map@10, top10, rank1, hit_rate
 
   Args:
-    array2d: matrix for distance. Note: negative value will be exclude.
+    array2d: matrix for distance. Note: negative value will be excluded.
     label_query: query label
     label_ref: ref label
     topk: k value for map, usually we set topk 10, if topk is set 1,
@@ -22,13 +22,14 @@ def calc_map(array2d: np.ndarray, label_query: List, label_ref: List,
     verbose: logging level, 0 for no log, 1 for print ap of every query
 
   Returns:
-    MAP, top10, rank1
+    MAP, top10, rank1, hit_rate
 
   Notes:
-    P@k: for given query and k, top k result has (P@k) positive sample(right res)
-    AP@k: = A(P@k), for given k, if kth of rank res is pos, choose it.
-            A(P@k) is to compute average of choosed result
-    MAP@k: mean of all classify of (Ap@k)
+    P@k: for a given query and k as a rank# within ranked query results,
+         top k results have a total of (P@k) positive samples (correct classifications)
+    AP@k: = A(P@k), for given k, if the kth member of ranked results is correct, choose it.
+            A(P@k) is to compute average of chosen results
+    MAP@k: mean of (Ap@k) across all classes
 
   References:
     mAP definition: https://towardsdatascience.com/breaking-down-mean-average-precision-map-ae462f623a52#f9ce
