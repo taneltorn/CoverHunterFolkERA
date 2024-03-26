c282f62c99bf7aaa5e50869b6a9189a8a814ffa3
Alan Ng <15185920+alanngnet@users.noreply.github.com>
2024-03-26 17:08:41 -0500

    changed t-SNE output default path to model_dir

--- tools/eval_testset.py
+++ tools/eval_testset.py
@@ -23,7 +23,7 @@ def _main():
   parser.add_argument('query_path')
   parser.add_argument('ref_path')
   parser.add_argument('-query_in_ref_path', default='', type=str)
-  parser.add_argument('-plot_name', default='', type=str, help='Save a t-SNE plot of the distance matrix to this path')
+  parser.add_argument('-plot_name', default='', type=str, help='Save a t-SNE plot of the distance matrix to this path. Default path is model_dir if plot_name is just a filename.')
   parser.add_argument('-test_only_labels', default='', type=str, help='Path to list of song_ids reserved for test dataset for use in t-SNE plot.')
   parser.add_argument('-dist_name', default='', type=str, help='Save the distance matrix to this path')
 
@@ -56,16 +56,27 @@ def _main():
   epoch = model.load_model_parameters(checkpoint_dir, device=device)
 
   embed_dir = os.path.join(model_dir, "embed_{}_{}".format(epoch, "tmp"))
+  
+  if args.plot_name:
+    plot_name = args.plot_name
+    path = os.path.dirname(plot_name)
+    if path!= '':
+      assert os.path.isdir(path), f"Invalid plot path: {plot_name}"
+    else:
+      # put the plot in model_dir as default location
+      plot_name = os.path.join(model_dir, plot_name)
+  else: 
+    plot_name = '' 
 
   if args.test_only_labels:
-     # convert list of song IDs from strings to integers as _cluster_plot() expects
-     test_only_labels = [int(n) for n in read_lines(args.test_only_labels)]
+    # convert list of song IDs from strings to integers as _cluster_plot() expects
+    test_only_labels = [int(n) for n in read_lines(args.test_only_labels)]
   
   mean_ap, hit_rate, rank1 = eval_for_map_with_feat(
     hp, model, embed_dir, query_path=query_path,
     ref_path=ref_path, query_in_ref_path=query_in_ref_path,
     batch_size=64, logger=logger, test_only_labels=test_only_labels, 
-    plot_name=args.plot_name, dist_name=args.dist_name)
+    plot_name=plot_name, dist_name=args.dist_name)
       
   return
 
