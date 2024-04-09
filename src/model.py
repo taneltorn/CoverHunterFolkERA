696a49d3dc3159a4046694de1298648b3b87a3a7
Samuel Gauthier <samuel.gauthier@gmail.com>
2024-04-09 22:08:05 +0200

    model: merge basicmodel
    It complexifies things for nothing, let's just merge the BasicModel
    class in the Model class and remove src/basic_model.py.

--- src/model.py
+++ src/model.py
@@ -8,10 +8,10 @@ import numpy as np
 import torch
 import torch.nn.functional as F
 
-from src.basic_model import BasicModel
 from src.loss import CenterLoss, FocalLoss, HardTripletLoss
 from src.module.conformer import ConformerEncoder
 from src.module.layers import Conv1d, Linear
+from src.pytorch_utils import get_latest_model, get_model_with_epoch
 from src.utils import load_hparams
 
 
@@ -163,7 +163,7 @@ class AttentiveStatisticsPooling(torch.nn.Module):
         return torch.as_tensor(mask, dtype=dtype, device=device)
 
 
-class Model(BasicModel):
+class Model(torch.nn.Module):
     """Csi Backbone Model:
         Batch-norm
         Conformer-Encoder(x6 or x4)
@@ -179,8 +179,10 @@ class Model(BasicModel):
     """
 
     def __init__(self, hp: Dict) -> None:
-        super(BasicModel, self).__init__()
+        super().__init__()
         self._hp = hp
+        self._epoch = 0
+        self._step = 0
         self._global_cmvn = torch.nn.BatchNorm1d(hp["input_dim"])
         self._encoder = ConformerEncoder(
             input_size=hp["input_dim"],
@@ -230,6 +232,47 @@ class Model(BasicModel):
             device=hp["device"],
         )
         logging.info(f"Model size: {self.model_size() / 1000 / 1000:.3f}M \n")
+
+    def load_model_parameters(
+        self, model_dir, epoch_num=-1, device="mps", advanced=False,
+    ):
+        """load parameters from pt model, and return model epoch,
+        if advanced, model can has different variables from saved"""
+        if epoch_num == -1:
+            model_path, epoch_num = get_latest_model(model_dir, "g_")
+        else:
+            model_path = get_model_with_epoch(model_dir, "g_", epoch_num)
+            assert model_path, f"Error:model with epoch {epoch_num} not found"
+
+        state_dict_g = torch.load(model_path, map_location=device)["generator"]
+        if advanced:
+            model_dict = self.state_dict()
+            valid_dict = {
+                k: v for k, v in state_dict_g.items() if k in model_dict
+            }
+            model_dict.update(valid_dict)
+            self.load_state_dict(model_dict)
+            for k in model_dict:
+                if k not in state_dict_g:
+                    logging.warning(f"{k} not be initialized")
+        else:
+            self.load_state_dict(state_dict_g)
+
+        self.eval()
+        self._epoch = epoch_num
+        logging.info(
+            f"Successful init model with epoch-{self._epoch}, device:{device}\n",
+        )
+        return self._epoch
+
+    def save_model_parameters(self, g_checkpoint_path) -> None:
+        torch.save({"generator": self.state_dict()}, g_checkpoint_path)
+
+    def get_epoch_num(self):
+        return self._epoch
+
+    def get_step_num(self):
+        return self._step
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """feat[b, frame_size, feat_size] -> embed[b, embed_dim]"""
@@ -283,6 +326,21 @@ class Model(BasicModel):
             embed = self.forward(feat)
             embed_ce = self._ce_layer(embed)
         return embed, embed_ce
+
+    # @torch.jit.export
+    def get_embed_length(self) -> int:
+        return self._hp["embed_dim"]
+
+    def get_ce_embed_length(self) -> int:
+        return self._hp["ce"]["output_dims"]
+
+    def model_size(self) -> int:
+        return sum(p.numel() for p in self.parameters())
+
+    def dump_torch_script(self, dump_path) -> None:
+        script_model = torch.jit.script(self)
+        script_model.save(dump_path)
+        logging.info(f"Export model successfully, see {dump_path}")
 
     @torch.jit.export
     def compute_embed(self, feat: torch.Tensor) -> torch.Tensor:
